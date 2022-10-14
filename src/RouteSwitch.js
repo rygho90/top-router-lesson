6e5a504520cdebc72d881752fb4702eae2d5750d
Ryan <rygho90@gmail.com>
2022-10-14 06:24:47 -0400

    Add my own route

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Profile from "./Profile";
+import TestPage from "./TestPage";
 
 export default function RouteSwitch() {
   return (
@@ -9,6 +10,7 @@ export default function RouteSwitch() {
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/test-page" element={<TestPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/TestPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function TestPage() {
+  return (
+    <div>
+      <h1>This is a test page</h1>
+      <p>I made my own route!</p>
+    </div>
+  );
+}
